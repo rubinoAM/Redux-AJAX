3ac0b9ad187df0c4fd84240d3da4200a1820d1d5
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-22 15:09:36 -0500

    Added note about fetchWeather & axios

--- src/actions/fetchWeather.js
+++ src/actions/fetchWeather.js
@@ -7,6 +7,8 @@ const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperia
 function fetchWeather(zip){
     //console.log(zip);
     const data = axios.get(weatherUrl+zip);
+    //We need reduxPromise in the main index because a .then() will never run here in this function
+
     return{
         type:'GET_WEATHER',
         payload:data,
